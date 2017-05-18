04604256f58e0c941fd933643e96069865e6ea2f
marioshehu <mario.shehu.07@gmail.com>
2017-05-18 14:53:58 +0200

    Add files via upload

---
+++ Server_Database/src/database/DatabaseConnection.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.sql.*; 
+
+/**
+ *
+ * @author nautilus
+ */
+public class DatabaseConnection {
+    
+    private Connection connection;
+    protected static Statement statement;
+    
+    public DatabaseConnection(String username, String password){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ProgettoIngSW?autoReconnect=true&useSSL=false",username,password);
+            this.statement = connection.createStatement();
+            
+        }catch(Exception ex){
+            System.out.println("DatabaseConnection ERROR:" + ex);
+        }
+    }
+}
---
+++ Server_Database/src/database/RMIDatabaseServer.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+/**
+ *
+ * @author nautilus
+ */
+public class RMIDatabaseServer {
+    
+    public static void main(String[] args) {
+       /*
+        MarioNOTE:
+        creating a connection to the database
+        */
+       DatabaseConnection connection = new DatabaseConnection("java", "password");
+       
+       try{
+           int port = 1099;
+           /*
+           MarioNOTE:
+           creating a Registry that allows the server to
+           publish a service and client to retrieve the proxy
+           */
+           Registry registry = LocateRegistry.createRegistry(port);
+           DatabaseGestion DBGestor = new DatabaseGestion();
+           registry.rebind("MyDatabase", DBGestor);
+           
+           System.out.println("\nServer is ready...");
+           
+       }catch(RemoteException ex){
+           System.out.println("\nRMIDatabaseServer ERROR: " + ex);
+       }
+    }
+    
+}
