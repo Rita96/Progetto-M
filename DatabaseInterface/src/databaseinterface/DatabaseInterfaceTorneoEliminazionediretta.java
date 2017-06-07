ba8e47971c5b1570c51e7bcae168a7888bd86d7c
nautilus <nautilus@mario-X550LA>
2017-06-07 22:50:14 +0200

    eliminazioneDiretta

--- DatabaseInterface/src/databaseinterface/DatabaseInterfaceTorneoEliminazionediretta.java
+++ DatabaseInterface/src/databaseinterface/DatabaseInterfaceTorneoEliminazionediretta.java
@@ -6,11 +6,24 @@
 package databaseinterface;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /**
  *
  * @author nautilus
  */
 public interface DatabaseInterfaceTorneoEliminazionediretta extends Remote{
+    void putTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, String nomeTorneo, int annoTorneo) throws RemoteException;
+    void putTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, boolean eliminazione, String nomeTorneo, int annoTorneo) throws RemoteException;
     
+    void updateFaseTorneoTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, String nomeTorneo, String annoTorneo, int nuoviPunti) throws RemoteException;
+    void updateNomeTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, String nomeTorneo, String annoTorneo, String nuovoNome) throws RemoteException;
+    void updateAnnoTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, String nomeTorneo, String annoTorneo, int nuovoAnno) throws RemoteException;
+    void updateNomeSquadraTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, String nomeTorneo, String annoTorneo, String nuovoNomeSquadra) throws RemoteException;
+    void updateCittaSquadraTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, String nomeTorneo, String annoTorneo, String nuovaCittaSquadra) throws RemoteException;
+    
+    String getTorneoEliminazionediretta() throws RemoteException;
+    
+    void deleteTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, String nomeTorneo, int annoTorneo) throws RemoteException;
+     
 }
