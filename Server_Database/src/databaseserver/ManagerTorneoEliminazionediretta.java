f2896de13a88d62bfcc81572611b1139abc0562e
nautilus <nautilus@mario-X550LA>
2017-06-07 22:48:57 +0200

    eliminazioneDiretta

--- Server_Database/src/databaseserver/ManagerTorneoEliminazionediretta.java
+++ Server_Database/src/databaseserver/ManagerTorneoEliminazionediretta.java
@@ -10,13 +10,129 @@ import databaseinterface.DatabaseInterfaceTorneoEliminazionediretta;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 
 /**
  *
  * @author nautilus
  */
 public class ManagerTorneoEliminazionediretta extends UnicastRemoteObject implements DatabaseInterfaceTorneoEliminazionediretta{
-    public ManagerTorneoEliminazionediretta() throws RemoteException{
+    
+        private static String query;       //where the query is written
+        private static ResultSet resSet;    //object needed to execute queries, and where the result of queries will be
+        private static ResultSetMetaData rsmd;  //object needed mainly to know the number of columns given by a certain query
         
-    }
+        public ManagerTorneoEliminazionediretta() throws RemoteException {
+            
+        }
+        
+        @Override
+        public void putTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, String nomeTorneo, int annoTorneo) throws RemoteException {
+            try{
+                query= "INSERT INTO TORNEO_ELIMINAZIONEDIRETTA (NOMESQUADRA, CITTASQUADRA, FASETORNEO, NOMETORNEO, ANNOTORNEO)\n "
+                        + "VALUES ( '" +  nomeSquadra + "' , '" + cittaSquadra + "' , '" + faseTorneo + "' , '" + nomeTorneo + "' , '" + annoTorneo + "' );";
+                PreparedStatement posted = DatabaseConnection.connection.prepareStatement(query);
+                posted.executeUpdate(query);
+            }catch(SQLException ex){
+                System.out.println("ERROR:" + ex);
+            } 
+        }
+        
+        @Override
+        public void putTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, boolean eliminazione, String nomeTorneo, int annoTorneo) throws RemoteException {
+            try{
+                query= "INSERT INTO TORNEO_ELIMINAZIONEDIRETTA\n "
+                        + "VALUES ( '" +  nomeSquadra + "' , '" + cittaSquadra + "' , '" + faseTorneo + "' , '" + eliminazione + "' , '" + nomeTorneo + "' , '" + annoTorneo + "' );";
+                PreparedStatement posted = DatabaseConnection.connection.prepareStatement(query);
+                posted.executeUpdate(query);
+            }catch(SQLException ex){
+                System.out.println("ERROR:" + ex);
+            } 
+        }
+        
+        @Override
+        public void updateFaseTorneoTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, String nomeTorneo, String annoTorneo, int nuoviPunti) throws RemoteException {
+            try{
+                query= "UPDATE TORNEO_ELIMINAZIONEDIRETTA\n "
+                        + "SET FASETORNEO = '" + nuoviPunti + "'\n "
+                        + "WHERE NOMESQUADRA = '" + nomeSquadra + "' AND CITTASQUADRA = '" + cittaSquadra + "' AND FASETORNEO = '" + faseTorneo + "' AND NOMETORNEO = '" + nomeTorneo + "' AND ANNOTORNEO = '" + annoTorneo + "' ;";
+                PreparedStatement posted = DatabaseConnection.connection.prepareStatement(query);
+                posted.executeUpdate(query);
+            }catch(SQLException ex){
+                System.out.println("ERROR:" + ex);
+            }
+        }
+
+        @Override
+        public void updateNomeTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, String nomeTorneo, String annoTorneo, String nuovoNome) throws RemoteException {
+            try{
+                query= "UPDATE TORNEO_ELIMINAZIONEDIRETTA\n "
+                        + "SET NOMETORNEO = '" + nuovoNome + "'\n "
+                        + "WHERE NOMESQUADRA = '" + nomeSquadra + "' AND CITTASQUADRA = '" + cittaSquadra + "' AND FASETORNEO = '" + faseTorneo + "' AND NOMETORNEO = '" + nomeTorneo + "' AND ANNOTORNEO = '" + annoTorneo + "' ;";
+                PreparedStatement posted = DatabaseConnection.connection.prepareStatement(query);
+                posted.executeUpdate(query);
+            }catch(SQLException ex){
+                System.out.println("ERROR:" + ex);
+            }
+        }
+
+        @Override
+        public void updateAnnoTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, String nomeTorneo, String annoTorneo, int nuovoAnno) throws RemoteException {
+            try{
+                query= "UPDATE TORNEO_ELIMINAZIONEDIRETTA\n "
+                        + "SET ANNOTORNEO = '" + annoTorneo + "'\n "
+                        + "WHERE NOMESQUADRA = '" + nomeSquadra + "' AND CITTASQUADRA = '" + cittaSquadra + "' AND FASETORNEO = '" + faseTorneo + "' AND NOMETORNEO = '" + nomeTorneo + "' AND ANNOTORNEO = '" + annoTorneo + "' ;";
+                PreparedStatement posted = DatabaseConnection.connection.prepareStatement(query);
+                posted.executeUpdate(query);
+            }catch(SQLException ex){
+                System.out.println("ERROR:" + ex);
+            }
+        }
+
+        @Override
+        public void updateNomeSquadraTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, String nomeTorneo, String annoTorneo, String nuovoNomeSquadra) throws RemoteException {
+            try{
+                query= "UPDATE TORNEO_ELIMINAZIONEDIRETTA\n "
+                        + "SET NOMESQUADRA = '" + nuovoNomeSquadra + "'\n "
+                        + "WHERE NOMESQUADRA = '" + nomeSquadra + "' AND CITTASQUADRA = '" + cittaSquadra + "' AND FASETORNEO = '" + faseTorneo + "' AND NOMETORNEO = '" + nomeTorneo + "' AND ANNOTORNEO = '" + annoTorneo + "' ;";
+                PreparedStatement posted = DatabaseConnection.connection.prepareStatement(query);
+                posted.executeUpdate(query);
+            }catch(SQLException ex){
+                System.out.println("ERROR:" + ex);
+            }
+        }
+
+        @Override
+        public void updateCittaSquadraTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, String nomeTorneo, String annoTorneo, String nuovaCittaSquadra) throws RemoteException {
+            try{
+                query= "UPDATE TORNEO_ELIMINAZIONEDIRETTA\n "
+                        + "SET CITTASQUADRA = '" + nuovaCittaSquadra + "'\n "
+                        + "WHERE NOMESQUADRA = '" + nomeSquadra + "' AND CITTASQUADRA = '" + cittaSquadra + "' AND FASETORNEO = '" + faseTorneo + "' AND NOMETORNEO = '" + nomeTorneo + "' AND ANNOTORNEO = '" + annoTorneo + "' ;";
+                PreparedStatement posted = DatabaseConnection.connection.prepareStatement(query);
+                posted.executeUpdate(query);
+            }catch(SQLException ex){
+                System.out.println("ERROR:" + ex);
+            }
+        }
+        
+        @Override
+        public String getTorneoEliminazionediretta() throws RemoteException {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+        @Override
+        public void deleteTorneoEliminazionediretta(String nomeSquadra, String cittaSquadra, int faseTorneo, String nomeTorneo, int annoTorneo) throws RemoteException {
+            try{
+                query= "DELETE FROM TORNEO_ELIMINAZIONEDIRETTA\n "
+                        + "WHERE NOMESQUADRA = '" + nomeSquadra + "' AND CITTASQUADRA = '" + cittaSquadra + "' AND FASETORNEO = '" + faseTorneo + "' AND NOMETORNEO = '" + nomeTorneo + "' AND ANNOTORNEO = '" + annoTorneo +"' ;";
+                PreparedStatement posted = DatabaseConnection.connection.prepareStatement(query);
+                posted.executeUpdate(query);
+            }catch(SQLException ex){
+                System.out.println("ERROR:" + ex);
+            } 
+        }
+
 }
