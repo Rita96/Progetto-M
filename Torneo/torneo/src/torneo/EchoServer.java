999eda7768e02dc23ae176c1b7be2a97e255b02b
Giulia <Giulia@nyaell>
2017-06-19 19:58:55 +0200

    aggiunta di socket server per gestire un generatore in client per aggiunta e rimozione di arbitri

---
+++ Torneo/torneo/src/torneo/EchoServer.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package torneo;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+/**
+ *
+ * @author giadamarconi
+ */
+public class EchoServer 
+{
+    private boolean connessione = false;
+    private Socket socket;
+    private int port;
+    private ServerSocket serverSocket;
+    private List<String> parametri = new ArrayList<>(); //Contiene i parametri passati dalla comunicazione attuale
+    private String messaggio;
+    PrintWriter out;
+    
+    
+    public EchoServer(int port) 
+    {
+        this.port = port; 
+    }
+    
+       public boolean startServer()  
+       {
+           try
+           {
+                serverSocket = new ServerSocket(port); // apro una porta TCP
+                System.out.println("Socket aperto sulla porta: " + port); //resto in attesa di una connessione
+                socket = serverSocket.accept(); 
+                System.out.println("Connessione client accettata.");
+                return this.connessione = true;
+           } 
+           catch(IOException e)
+           {
+               System.out.println("Non riesco a connettermi al socket.");
+               return this.connessione = false;
+           }    
+       }
+            
+        public void chiudiSocket(Socket socket, Scanner in, PrintWriter out) 
+        {
+            try {
+            System.out.println("I socket stanno per chiudersi.."); 
+            in.close();
+            out.close();
+            socket.close();
+            serverSocket.close();
+            this.connessione = false;
+            }
+            catch(Exception e)
+            {
+                System.out.println("Errore nella chiusura dei socket e degli stream.");
+            }
+        }
+        
+        public void splitLine(String line)
+        {
+            String[] tmp = null; //Separare ogni argomento con uno spazio!!!!!!!
+            tmp = line.split(" "); //Passo da un array normale ad una collection per comodità personale
+            this.parametri.clear();
+            for(int i=0; i<tmp.length; i++)
+            {
+                this.parametri.add(tmp[i]);
+                System.out.println(this.parametri.get(i));
+            }
+        }
+        
+        public boolean controllaSocket() throws IOException
+        {
+            Scanner in = new Scanner(socket.getInputStream()); // apro gli stream di input e output per leggere
+            out = new PrintWriter(socket.getOutputStream());
+            while (true) 
+            {      
+                String line = in.nextLine(); //Dati che vengono forniti
+                
+                if (line.equals("Chiudi")) // se scrivono chiudi, avvio la procedura di chiusura della connessione
+                {
+                    chiudiSocket(socket, in, out);
+                    return this.connessione;
+                } 
+                else 
+                {
+                    out.println("Ho ricevuto: " + line); //ricevo correttamente, mando allo splitting
+                    splitLine(line);
+                    out.flush();
+                    return this.connessione = true;
+                }
+            }
+        }
+
+        public List<String> getParametri() 
+        {
+            return parametri;
+        } 
+
+        public void setMessaggio(String messaggio) 
+        {
+            this.messaggio = messaggio;
+        }
+        
+        public void mandaMessaggio() throws IOException
+        {
+            out = new PrintWriter(socket.getOutputStream());    
+            out.println(this.messaggio);
+            out.flush();
+        }
+   
+}
--- Torneo/torneo/src/torneo/Test.java
+++ Torneo/torneo/src/torneo/Test.java
@@ -12,10 +12,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 
 /**
@@ -25,163 +28,86 @@ import javax.swing.JFrame;
 public class Test {
     static DatabaseInterfaceFactory q;
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        //PRIMO TEST
-//        Giocatore g1s1 = new Giocatore("Prova", "Prova", 1);
-//        Giocatore g2s1 = new Giocatore("Prova2", "Prova2", 2);
-//        Giocatore g1s2 = new Giocatore("Prova3", "Porva3", 3);
-//        Giocatore g2s2 = new Giocatore("Prova4", "Prova4", 4);
-//        List<Giocatore> l1 = new ArrayList<>();
-//        l1.add(g1s1);
-//        l1.add(g2s1);
-//        List<Giocatore> l2 = new ArrayList<>();
-//        l2.add(g1s2);
-//        l2.add(g2s2);
-//        Giocatore g2s3 = new Giocatore("Prova", "prova", 5);
-//        Squadra s1 = new Squadra("S1", "blu", "Milano", l1);
-//        Squadra s2 = new Squadra("S2", "rosso", "Milano", l2);
-//        Arbitro a = new Arbitro("A1", "A2", "cod", "pass");
-//        Partita p = new Partita(s1,s2, a, "Napoli");
-//        a.logIn("cod", "pass");
-//        try{ 
-//            p.setCartellino(ColoreCartellino.ROSSO, g2s3);
-//        } catch (GiocatoreInesistenteException e){
-//            System.out.println(e.getMessage());
-//        }
-//        try{
-//            p.setPuntiSquadra(-2, "S3");
-//        } catch(RuntimeException e){
-//            System.out.println(e.getMessage());
-//        }
-
-        // TEST DEL GENERATORE DEL TORNEO
-//        BufferedReader br = new BufferedReader(new FileReader(new File("files/squadre.txt")));
-//        List<Squadra> listaSquadre = new ArrayList<>();
-//        List<Giocatore> l = new ArrayList<>();
-//        while(br.ready()){
-//            String[] ls = br.readLine().split("\t");
-//            if(ls[0].equals("-")){
-//                Squadra s = new Squadra(ls[1], ls[2], ls[3], l);
-//                listaSquadre.add(s);
-//                l.clear();
-//            } else {
-//                Giocatore g = new Giocatore(ls[0], ls[1], Integer.parseInt(ls[2]));
-//                l.add(g);
-//            }
-//        }
-//        BufferedReader br2 = new BufferedReader(new FileReader(new File("files/arbitri.txt")));
-//        List<Arbitro> listaArbitri = new ArrayList<>();
-//        while(br2.ready()){
-//            String[] ls = br2.readLine().split("\t");
-//            Arbitro a = new Arbitro(ls[0], ls[1], ls[2], ls[3]);
-//            listaArbitri.add(a);
-//        }
-//        
-//        GeneratoreTorneo gen = new GeneratoreTorneo("ITALIANA", listaSquadre, listaArbitri);
-//        
-        
-//        BufferedReader br = new BufferedReader(new FileReader(new File("files/squadre.txt")));
-//        Map<String, Squadra> listaSquadre =  new HashMap<>();
-//        List<Giocatore> l = new ArrayList<>();
-//        while(br.ready()){
-//            String[] ls = br.readLine().split("\t");
-//            if(ls[0].equals("-")){
-//                Squadra s = new Squadra(ls[1], ls[2], ls[3], l);
-//                listaSquadre.put(ls[1], s);
-//                l.clear();
-//            } else {
-//                Giocatore g = new Giocatore(ls[0], ls[1], Integer.parseInt(ls[2]));
-//                l.add(g);
-//            }
-//        }
-//        BufferedReader br2 = new BufferedReader(new FileReader(new File("files/arbitri.txt")));
-//        Map<String, Arbitro> listaArbitri =  new HashMap<>();
-//        while(br2.ready()){
-//            String[] ls = br2.readLine().split("\t");
-//            Arbitro a = new Arbitro(ls[0], ls[1], ls[2], ls[3]);
-//            a.logIn(ls[2], ls[3]);
-//            listaArbitri.put(ls[2], a);
-//        }
-//        BufferedReader br3 = new BufferedReader(new FileReader(new File("files/partite.txt")));
-//        List<Partita> listaPartite = new ArrayList<>();
-//        while(br3.ready()){
-//            String[] ls = br3.readLine().split("\t");
-//            if(ls[0].equals("-")){
-//                Partita p = new Partita(listaSquadre.get(ls[1]), listaSquadre.get(ls[2]), listaArbitri.get(ls[3]), ls[4], StatoPartita.valueOf(ls[5]));
-//                p.setStatoPartita(StatoPartita.valueOf(ls[5]));
-//                listaPartite.add(p);
-//            } else {
-//                listaPartite.get(listaPartite.size() - 1).setGoal(Integer.parseInt(ls[0]), ls[1], ls[2], Integer.parseInt(ls[3]));
-//            }
-//        }
-//       
-//        
-//        
-//        Italiana torneoItaliana = new Italiana("Calcio", listaPartite);
-//        ClassificaItaliana classificaItaliana = new ClassificaItaliana(torneoItaliana);
-//        System.out.println("\nClassifica Italiana \n" + classificaItaliana.toString());
-//        
-//        EliminazioneDiretta torneoElDiretta = new EliminazioneDiretta("nome", listaPartite);
-//        ClassificaEliminazioneDiretta classificaElDiretta = new ClassificaEliminazioneDiretta(torneoElDiretta);
-//        System.out.println("Classifica Eliminazione Diretta");
-//        for(Squadra s: classificaElDiretta.printRisultato()){
-//            System.out.println(s.getNome());
-//        }
         q = new DatabaseInterfaceFactory();
-        String nomeTorneo = "Calcio 1";
-        int anno = 2017;
-        Arbitro a = new Arbitro("a", "a", "a", "a", true);
-        Arbitro b = new Arbitro("b", "b", "b", "b", true);
-        Arbitro c = new Arbitro("c", "c", "c", "c", true);
-        
-        Giocatore g1s1 = new Giocatore("Prova", "Prova", 1);
-        Giocatore g2s1 = new Giocatore("Prova2", "Prova2", 2);
-        Giocatore g1s2 = new Giocatore("Prova3", "Porva3", 3);
-        Giocatore g2s2 = new Giocatore("Prova4", "Prova4", 4);
-        List<Giocatore> l1 = new ArrayList<>();
-        l1.add(g1s1);
-        l1.add(g2s1);
-        List<Giocatore> l2 = new ArrayList<>();
-        l2.add(g1s2);
-        l2.add(g2s2);
-        Giocatore g2s3 = new Giocatore("Prova", "prova", 5);
-        Squadra s1 = new Squadra("S1", "blu", "Milano", l1, true);
-        Squadra s2 = new Squadra("S2", "rosso", "Milano", l2, true);
-        Squadra s3 = new Squadra("S3", "rosso", "Milano", l2, true);
-        
-        int ID1 = 1;
-        int ID2 = 2;
-        int ID3 = 3;
-        int ID4 = 4;
-        int ID5 = 5;
-        int ID6 = 6;
-        int ID7 = 7;
-        
-        Partita p = new Partita(ID1, s1,s2, a, "Napoli", StatoPartita.PROGRAMMATA, nomeTorneo, anno, true);
-        
-        List<Partita> pa = new ArrayList<>();
-        pa.add(new Partita(ID2, s1, s2, a, "Milano", StatoPartita.REGOLAMENTARE, nomeTorneo, anno, true));
-        pa.add(new Partita(ID3, s1, s3, b, "Rimini", StatoPartita.SUPPLEMENTARI, nomeTorneo, anno, true));
-        pa.add(new Partita(ID4, s1, s2, c, "Aosta", StatoPartita.RIGORI, nomeTorneo, anno, true));
-        
-        List<Partita> pb = new ArrayList<>();
-        pb.add(new Partita(ID5, s3, s2, c, "Lecce", StatoPartita.TERMINATA, nomeTorneo, anno, true));
-        pb.add(new Partita(ID6, s1, s2, b, "Cagliari", StatoPartita.PROGRAMMATA, nomeTorneo, anno, true));
-        pb.add(new Partita(ID7, s1, s3, a, "Pavia", StatoPartita.RIGORI, nomeTorneo, anno, true));
-
-        EliminazioneDiretta torneoED = new EliminazioneDiretta(nomeTorneo, anno, pa, true);
-        Italiana torneoIT = new Italiana(nomeTorneo, anno, pb, true);
-        
         List<Torneo> tornei = new ArrayList<>();
-        tornei.add(torneoED);
-        tornei.add(torneoIT);
-        
+        try {
+                tornei = Test.q.makeTorneoTable().getTorneo();
+        } catch (RemoteException ex) {
+            Logger.getLogger(Torneo.class.getName()).log(Level.SEVERE, null, ex);
+        }
         GeneratoreTorneo gentorneo = new GeneratoreTorneo("g", "g", "g", "g");
         
         JFrame sceltatorneo = new SceltaTorneoGUI(tornei, gentorneo);
         sceltatorneo.setSize(1000, 655);
         sceltatorneo.setLocation(400, 250);
+        
+                //----------------------------------------------------------------------
+        // Inizio client-server
+        
+        boolean connessione = false;
+        EchoServer server = new EchoServer(4020); 
+        
+        try 
+        {
+            connessione = server.startServer();
+            server.setMessaggio("Inserire parametri per generatore: (cf password)");
+            server.mandaMessaggio();
+            connessione = server.controllaSocket();
+            
+        } 
+        catch (IOException e) 
+        {
+            System.err.println(e.getMessage()); 
+        }
+        
+        gentorneo.logIn(server.getParametri().get(0), server.getParametri().get(1));
+        
+        System.out.println("Stato di connessione:"+connessione);
+        
+        if(gentorneo.getAutenticazione().equals("AUTENTICATO")){
+
+            
+            server.setMessaggio("Inserire numero per indicare scelta: 1: aggiungi arbitro 2: rimuovi arbitro");
+            server.mandaMessaggio();
+            connessione = server.controllaSocket();
+
+
+            while(connessione)
+            {
+                switch(server.getParametri().get(0))
+                {
+                    case "1": //Aggiungi arbitro
+                    {
+                        server.setMessaggio("Inserire parametri per definizione nuovo utente arbitro: (nome cognome username password)");
+                        server.mandaMessaggio();
+                        connessione = server.controllaSocket();
+                        Arbitro arbitro = new Arbitro(server.getParametri().get(0), server.getParametri().get(1), server.getParametri().get(2), server.getParametri().get(3), true);
+                        
+                    } break;
+
+                    case "2": //Rimuovi arbitro
+                    {
+                        server.setMessaggio("Inserire parametri per dell'utente arbitro da rimuovere: (nome cognome username password)");
+                        server.mandaMessaggio();
+                        connessione = server.controllaSocket();
+                        Arbitro arbitro = new Arbitro(server.getParametri().get(0), server.getParametri().get(1), server.getParametri().get(2), server.getParametri().get(3), false);
+                        try {
+                            Test.q.makeArbitroTable().deleteArbitro(arbitro);
+                        } catch (RemoteException ex) {
+                            Logger.getLogger(Arbitro.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    } break;
+
+                    default: 
+                    {
+                        server.setMessaggio("Scelta non contemplata.");
+                        server.mandaMessaggio();
+                    } break;
+                }
+            }
+
+            System.out.println("Il socket è stato chiuso.");
+        }
     }
-    
-    
-    
+   
 }
